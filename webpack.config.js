5e962400d5c91b997d9d6da31d42792382d603ba
Garrett Myrick <garrettmyrick@gmail.com>
2021-08-03 12:22:23 -0700

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -24,14 +24,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader',
-      },
-      {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'ts-loader'
       }
     ]
   },
