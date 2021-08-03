a1e7e59089a2bbf6b91526d8d897f50b390704b1
Garrett Myrick <garrettmyrick@gmail.com>
2021-08-03 11:03:56 -0700

    Added GraphQL to documents API

--- src/server/interfaces/documents-graphql-api.ts
+++ src/server/interfaces/documents-graphql-api.ts
@@ -1,4 +1,8 @@
+import gql from 'graphql-tag';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { IResolvers } from '@graphql-tools/utils'
 import type { CreateDocumentInput, DocumentInteractor, User } from '@/server/usecases';
+import { GraphQLSchema } from 'graphql';
 
 
 export class DocumentsGraphQLApi {
@@ -6,13 +10,45 @@ export class DocumentsGraphQLApi {
 
   constructor(interactor: DocumentInteractor) {
     this.interactor = interactor;
+    this.createDocument = this.createDocument.bind(this);
   };
 
 
-  getSchema() {
-    // Use imported typedefs
-    // Create a resolvers definition using this.<resolver>
-    // Create and return a schema using makeExecutableSchema
+  getSchema() : GraphQLSchema {
+    const typeDefs = gql`
+      type Author {
+        id: ID!
+        name: String!
+      }
+
+      type Document {
+        id: ID!
+        author: Author!
+        isPublic: Boolean!
+        title: String!
+        contentType: String!
+        content: String!
+      }
+
+      input CreateDocumentInput {
+        title: String,
+        contentType: String,
+        content: String
+      }
+
+      type Mutation {
+        createDocument(input: CreateDocumentInput!): Document
+      }
+    `;
+
+    const resolvers: IResolvers = {
+      Mutation: {
+        createDocument: this.createDocument
+      }
+    }
+    
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    return schema;
   }
 
 
