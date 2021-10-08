87b8b9f1d63e47fc31b344fd5f2c5df3fefae784
Garrett Myrick <garrettmyrick@gmail.com>
2021-10-08 09:17:20 -0700

    Converted DocumentContentTypeSelect to button+menu

--- src/react-web/components/document-content-type-select/document-content-type-select.tsx
+++ src/react-web/components/document-content-type-select/document-content-type-select.tsx
@@ -1,12 +1,13 @@
 import { useDocumentEditor } from '@/react-web/hooks';
-import { Select, SelectProps } from '@chakra-ui/react';
+import { Select, SelectProps, Button, ButtonProps, Menu, MenuItem, MenuList, MenuButton } from '@chakra-ui/react';
 import React from 'react';
 import { parseContentType } from '@/content/utils';
 import { ContentType } from '@/domain';
 import { useServices } from '@/react-web/services';
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 
-export type DocumentContentTypeSelectProps = SelectProps & {  };
+export type DocumentContentTypeSelectProps = {  };
 
 
 export function DocumentContentTypeSelect(props: DocumentContentTypeSelectProps) : JSX.Element {
@@ -34,10 +35,12 @@ export function DocumentContentTypeSelect(props: DocumentContentTypeSelectProps)
 
 
   // Change the content type
-  const setContentType = React.useCallback((event: React.ChangeEvent<HTMLSelectElement>) => {
+  const setContentType = React.useCallback((event: React.MouseEvent<HTMLDivElement>) => {
     (async () => {
+      const targetContentType = (event.target as HTMLButtonElement)?.value as string;
+
       const from = parseContentType(document!.contentType);
-      const to = parseContentType(event.target.value);
+      const to = parseContentType(targetContentType);
 
       const avalableMigrations = await services.migrations.getMigrationPaths(from, to);
       const migration = avalableMigrations[0];
@@ -51,20 +54,46 @@ export function DocumentContentTypeSelect(props: DocumentContentTypeSelectProps)
     })();
   }, [document?.contentType, document?.content]);
 
-
   return (
-    <Select
-      value={editor.document?.contentType}
-      onChange={setContentType}
-    >
-      {contentTypeOptions.map(contentType => (
-        <option
-          key={contentType.value} 
-          value={contentType.value}
-        >
-          {contentType.name}
-        </option>
-      ))}
-    </Select>
+    <Menu>
+      <MenuButton 
+        as={Button}
+        rightIcon={<ChevronDownIcon/>} 
+        disabled={!document?.contentType}
+        size='sm'
+        fontSize='xs'
+        color='gray.500'
+      >
+        {getButtonLabel(document?.contentType)}
+      </MenuButton>
+      <MenuList
+        onClick={setContentType}
+        shadow='2xl'
+        borderWidth='thin'
+        borderColor='gray.300'
+        color='black'
+      >
+        {contentTypeOptions.map(contentType => (
+          <MenuItem
+            key={contentType.value}
+            value={contentType.value}
+          >
+            {contentType.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
   );
+}
+
+
+function getButtonLabel(contentTypeValue?: string) : string {
+  const label = {
+    'text/plain': 'Text',
+    'text/html': 'HTML',
+    'application/json': 'JSON',
+    'application/json;format=slate001': 'Slate v1',
+  }[contentTypeValue ?? ''];
+
+  return label ?? contentTypeValue ?? 'Unknown';
 }
