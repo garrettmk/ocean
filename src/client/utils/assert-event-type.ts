4bd8fb1a8af278a85d655faa6a2cce2fd434d579
Garrett Myrick <garrettmyrick@gmail.com>
2021-10-11 22:03:18 -0700

    Bug fix: assertEventType now uses the template type's type

--- src/client/utils/assert-event-type.ts
+++ src/client/utils/assert-event-type.ts
@@ -2,7 +2,7 @@ import { ValidationError } from "@/domain";
 import { AnyEventObject } from "xstate";
 
 
-export function assertEventType<T extends AnyEventObject>(value: AnyEventObject, type: string) : asserts value is T {
+export function assertEventType<T extends AnyEventObject>(value: AnyEventObject, type: T['type']) : asserts value is T {
   if (value.type !== type)
     throw new ValidationError('Wrong event type', ['type'], type, value.type);
 }
