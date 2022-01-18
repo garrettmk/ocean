da7f1d195331d99cbd2eca628a29e85aec2969bb
Garrett Myrick <garrettmyrick@gmail.com>
2022-01-18 22:43:04 +0000

    graph-editor: added useNodeResizing()

---
+++ src/react-web/components/graph-editor/hooks/use-node-resizing.ts
@@ -0,0 +1,55 @@
+import React from 'react';
+import { useZoomPanHelper } from 'react-flow-renderer';
+
+
+export function useNodeResizing() {
+  const { project } = useZoomPanHelper();
+  const resizeHandle = React.useRef<HTMLDivElement | null>(null);
+  const resizeElement = React.useRef<HTMLDivElement | null>(null);
+
+  // Respond to mousemove events by changing the size of resizeElement
+  const resizeCallback = React.useCallback((event: MouseEvent) => {
+    if (!resizeElement.current)
+      return;
+
+    // Get the bounding rect of the element to resize
+    const elementRect = resizeElement.current.getBoundingClientRect();
+
+    // Translate both the element and mouse positions into react-flow's coordinate space
+    const { x: mouseX, y: mouseY } = project({ x: event.pageX, y: event.pageY });
+    const { x: elementX, y: elementY } = project({ x: elementRect.x, y: elementRect.y });
+
+    // Calculate the new width
+    const newWidth = mouseX - elementX;
+    const newHeight = mouseY - elementY;
+
+    // Set the element styles directly to avoid firing lots of re-renders
+    resizeElement.current.style.width = `${newWidth}px`;
+    resizeElement.current.style.height = `${newHeight}px`;
+  }, []);
+
+  // Remove the mouse event listener
+  const stopResizing = React.useCallback(() => {
+    window.removeEventListener('mousemove', resizeCallback);
+  }, [resizeCallback]);
+
+  // While resizing, listen to mouse events on the window
+  // If mouseup, stop resizing
+  const startResizing = React.useCallback(() => {
+    window.addEventListener('mousemove', resizeCallback);
+    window.addEventListener('mouseup', stopResizing);
+  }, [resizeCallback]);
+
+  // Listen for mousedown events on resizeHandle
+  React.useLayoutEffect(() => {
+    if (resizeHandle.current)
+      resizeHandle.current.addEventListener('mousedown', startResizing);
+    
+    return () => {
+      resizeHandle.current?.removeEventListener('mousedown', startResizing);
+    }
+  }, [resizeHandle.current]);
+
+
+  return [resizeHandle, resizeElement];
+}
