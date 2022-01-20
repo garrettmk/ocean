4fced2e64bfdeef700332537ebfedf8b1976575a
Garrett Myrick <garrettmyrick@gmail.com>
2022-01-20 07:06:03 +0000

    increased drag handle size

--- src/react-web/components/drag-handle/drag-handle.tsx
+++ src/react-web/components/drag-handle/drag-handle.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Box, BoxProps } from '@chakra-ui/react';
 
+export type DragHandleProps = BoxProps;
 
 export const DragHandle = React.forwardRef<HTMLDivElement, BoxProps>(function DrageHandle(props, ref) {
   return (
     <Box
         ref={ref}
-        width='8px'
-        height='8px'
+        width='16px'
+        height='16px'
         overflow='hidden'
         borderBottomRightRadius='3px'
         {...props}
       >
         <Box
           borderStyle='solid'
-          borderWidth='4px'
+          borderWidth='8px'
           borderColor='transparent'
           borderBottomColor='gray.600'
           borderRightColor='gray.600'
